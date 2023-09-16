b7a13cb363150050366cc783b776de334afe97f7
PUSHRSP <pushrsp@gmail.com>
2023-09-16 11:09:47 +0900

    ADD: 기본 틀 작성

--- src/main/java/me/project/Main.java
+++ src/main/java/me/project/Main.java
@@ -1,14 +1,12 @@
 package me.project;
 
-import me.project.config.Configuration;
-import me.project.config.ConfigurationManager;
+import me.project.server.JapressServer;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/config.json");
-        Configuration currentConfiguration = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("port: " + currentConfiguration.getPort());
-        System.out.println("webroot: " + currentConfiguration.getWebroot());
+    public static void main(String[] args) throws IOException {
+        JapressServer.run(8080);
     }
 }
---
+++ src/main/java/me/project/server/JapressServer.java
@@ -0,0 +1,107 @@
+package me.project.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
+public class JapressServer {
+
+    private ServerSocketChannel serverSocketChannel;
+    private Selector selector;
+    private final int port;
+
+    private volatile boolean running = true;
+
+    public static void run(int port) throws IOException {
+        new JapressServer(port).run();
+    }
+
+    private JapressServer(int port) throws IOException {
+        this.port = port;
+
+        openSelector();
+        openChannel();
+    }
+
+    private void openSelector() throws IOException {
+        this.selector = Selector.open();
+    }
+
+    private void openChannel() throws IOException {
+        this.serverSocketChannel = ServerSocketChannel.open();
+        this.serverSocketChannel.configureBlocking(false);
+        this.serverSocketChannel.bind(new InetSocketAddress(this.port));
+        this.serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+    }
+
+    private void run() {
+        while (this.running) {
+            SelectionKey selectionKey = null;
+            try {
+                this.selector.select(300L);
+
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = keys.iterator();
+                while (iterator.hasNext()) {
+                    selectionKey = iterator.next();
+
+                    if(selectionKey.isAcceptable()) {
+                        accept(selectionKey);
+                    } else if(selectionKey.isReadable()) {
+                        read(selectionKey);
+                    } else if(selectionKey.isWritable()) {
+                        write(selectionKey);
+                    }
+
+                    iterator.remove();
+                }
+            } catch (IOException e) {
+                close(selectionKey);
+            }
+        }
+
+        clean();
+    }
+
+    private void clean() {
+        for (SelectionKey key : this.selector.keys()) {
+            if(Objects.isNull(key.attachment())) {
+                continue;
+            }
+
+            System.out.println("HI");
+        }
+    }
+
+    private void close(SelectionKey selectionKey) {
+        if(Objects.isNull(selectionKey)) {
+            return;
+        }
+
+        if(Objects.isNull(selectionKey.channel())) {
+            return;
+        }
+
+        // TODO
+        Object attachment = selectionKey.attachment();
+
+        selectionKey.cancel();
+    }
+
+    private void accept(SelectionKey selectionKey) throws IOException {
+        SocketChannel client = this.serverSocketChannel.accept();
+        client.configureBlocking(false);
+        client.register(selectionKey.selector(), SelectionKey.OP_READ);
+    }
+
+    private void read(SelectionKey selectionKey) {
+
+    }
+
+    private void write(SelectionKey selectionKey) {
+
+    }
+}
