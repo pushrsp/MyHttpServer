630e0cc14889d404d9256bcea3dce11a3aedb991
PUSHRSP <pushrsp@gmail.com>
2023-10-07 14:01:29 +0900

    요청 파싱 에러 추가

---
+++ src/main/java/me/project/exception/ParseException.java
@@ -0,0 +1,19 @@
+package me.project.exception;
+
+public class ParseException extends RuntimeException {
+
+    public ParseException() {
+    }
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+    public ParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParseException(Throwable cause) {
+        super(cause);
+    }
+}
